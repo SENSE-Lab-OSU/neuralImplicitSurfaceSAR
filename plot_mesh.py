0ae87e888df7f4ebc6919c3302698a993ca3da60
nithin-pi <sugavanam.3@osu.edu>
2023-02-27 17:38:33 -0500

    Added main scripts

---
+++ plot_mesh.py
@@ -0,0 +1,8 @@
+import numpy as np
+import open3d as o3d
+import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+
+
+mesh = o3d.io.read_triangle_mesh('exp/parkingLot_tractor_new/0000024500_mesh.ply')
+
