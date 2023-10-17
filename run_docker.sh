4eb76e3f42194afc568c9e3d87a0815bf8b9957f
yuyichang <22040659+YuyiChang@users.noreply.github.com>
2023-10-17 16:48:19 +0000

    add run_docker

---
+++ run_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+MOUNT_OPS="-v $(pwd):/workspace"
+OPTS=""
+
+IMAGE="complex_sar"
+REMOTE_IMAGE="agilemelon/complex_sar:torch113-cuda118"
+
+
+if [ $# -eq 0 ]; then
+    docker run -it --rm --gpus all $MOUNT_OPS $OPTS $REMOTE_IMAGE
+elif [ "$1" = "local" ]; then
+    docker run -it --rm --gpus all $MOUNT_OPS $OPTS $IMAGE
+elif [ "$1" = "cpu" ]; then
+    docker run -it --rm $MOUNT_OPS $OPTS $IMAGE
+elif [ "$1" = "build" ]; then
+    docker build -t $IMAGE -f docker/Dockerfile .
+else
+    echo "Unknown argument: $1"
+fi
